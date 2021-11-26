23a3f6910e8464a1e4f5b4ad635d4df3086317cb
Trung <trung.nt.e@gmail.com>
2021-11-26 23:59:53 +0700

    Change port web and api, hardcode mongo uri, change version mongoose

--- api/models/init.js
+++ api/models/init.js
@@ -2,14 +2,21 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-mongoose.connect(process.env.MONGO_URI, { useMongoClient: true })
+// mongoose.connect(process.env.MONGO_URI, { useMongoClient: true })
+
+// mongooes.connect('mongodb://root:example@mongo:27017/test_my_database?authSource=admin', 
+//     { useNewUrlParser: true })
+//     .then(() => console.log('Connected to MongoDB...'))
+//     .catch(err => console.error('Could not connect to MongoDB...', err));
+
+mongoose.connect('mongodb://root:example@mongo:27017/booking_database?authSource=admin', { useNewUrlParser: true })
 
   .then(() => {
     console.log('Successfully connected to database')
   })
 
   .catch(error => {
-    console.error('Error connecting to MongoDB database', error)
+    console.error('>>>> Error connecting to MongoDB database - error: ', error)
   })
 
 module.exports = mongoose
